a804840d5dd8753ad8d1f917a21b20f86fb0f7a7
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-01-29 13:52:34 +0800

    feat(db): add uuid dependency and create users table schema with indexes

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,2 +1,39 @@
+import { sql } from "drizzle-orm";
+import {
+  index,
+  pgTableCreator,
+  timestamp,
+  unique,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { v7 as uuidv7 } from "uuid";
 
+export const createTable = pgTableCreator((name) => `${name}`);
 
+export const users = createTable(
+  "users",
+  {
+    id: uuid("id")
+      .primaryKey()
+      .notNull()
+      .$default(() => uuidv7()),
+    username: varchar("username", { length: 50 }).notNull(),
+    password: varchar("password", { length: 150 }).notNull(),
+    createdAt: timestamp("created_at", {
+      withTimezone: true,
+      mode: "string",
+    })
+      .$default(() => sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", {
+      withTimezone: true,
+      mode: "string",
+    }),
+  },
+  (table) => [
+    unique("username_unique").on(table.username),
+    index("username_idx").using("btree", table.username),
+    index("user_idx").using("btree", table.id),
+  ],
+);
